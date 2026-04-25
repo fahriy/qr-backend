f3e09c3b84d1ac60834edc8490d83677f8807141
fahriy <44180175+fahriy@users.noreply.github.com>
2026-04-26 01:29:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ async function handleRequest(req, res) {
 🌐 Zaman Dilimi: ${timezone}`;
 
     // 6. Telegram'a gönder (BOT_TOKEN'ını değiştir!)
-    await fetch(`https://api.telegram.org/bot***/sendMessage`, {
+    await fetch(`https://api.telegram.org/bot8381262942:AAGb0yeCVcl4-IPL_dAhxm7lOjdE7DEKTaA/sendMessage`, {
       method: "POST",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify({
